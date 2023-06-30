d1c8133f421d9ced78de1c6079b4497785b82a34
yakohere <yakohere@gmail.com>
2023-06-30 12:56:40 +0200

    faq update

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -3,15 +3,25 @@ import "./Faq.css"
 const Faq = () => {
     return (
         <ul className="faq">
+            <li className="faq__item">You can ask these questions about these: London, Paris, Berlin, Rome, Prague, Warsaw</li>
             <li className="faq__item">Where to see in Berlin?</li>
             <li className="faq__item">Tell me more about Museum Island?</li>
+            <li className="faq__item">WHat is the description of Museum Island?</li>
             <li className="faq__item">What is the price of Museum Island?</li>
+            <li className="faq__item">What is the rating of Museum Island?</li>
             <li className="faq__item">What is the working hours of Museum Island?</li>
+            <li className="faq__item">Where is the location of Museum Island?</li>
+            
             <li className="faq__item">What to eat in Berlin?</li>
             <li className="faq__item">Tell me more about Berliner Pfannkuchen?</li>
+            <li className="faq__item">What is the price of Berliner Pfannkuchen?</li>
+            <li className="faq__item">Where can I eat Berliner Pfannkuchen?</li>
+
             <li className="faq__item">What activities I can do in Berlin?</li>
-            <li className="faq__item">Tell me more about Tour of Historical Sites?</li>
-            <li className="faq__item">We support: London, Paris, Berlin, Rome, Prague, Warsaw</li>
+            <li className="faq__item">Tell me about Tour of Historical Sites?</li>
+            <li className="faq__item">What items I need for Tour of Historical Sites?</li>
+            <li className="faq__item">What is the price of Historical Sites?</li>
+            <li className="faq__item">Give me tips for Historical Sites</li>
         </ul>
     )
 }
